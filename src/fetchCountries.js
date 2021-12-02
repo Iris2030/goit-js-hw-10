da0817917e48e05183ea112a96e11f7376d61d19
Iris2030 <ibazhinova@gmail.com>
2021-12-02 22:36:17 +0200

    first commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+
+// export function fetchCountries(countryName){
+//     fetch(`https://restcountries.com/v3.1/name/Ukraine`).then(response =>
+// { return response.json()
+// }).then(country => {
+//     console.log((country));
+//     const markup = template(country)
+//     console.log(markup);
+//     countriesList.innerHTML = markup
+// }).catch(error => {
+//     console.log(error);
+// })
+// }
+ 
+//  fetchCountries()
+ 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './sass/main.scss';
+import './css/styles.css';
+import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix'
+import template from './template.hbs'
+// import fetchCountries from './fetchCountries'
+
+const countriesList = document.querySelector('.country-list')
+const DEBOUNCE_DELAY = 300;
+console.log(countriesList);
+
+
+ function fetchCountries(countryName){
+    fetch(`https://restcountries.com/v3.1/name/Ukraine`).then(response =>
+{ return response.json()
+})
+}
+ 
+fetchCountries().then(renderMarkup).catch(error => {
+    console.log(error);
+})
+ 
+function renderMarkup(country) {
+        const markup = template(country)
+     countriesList.innerHTML = markup
+}
