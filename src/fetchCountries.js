1287de1f51c2148b6465ee0c12dbfea4dffb173b
Iris2030 <ibazhinova@gmail.com>
2021-12-05 10:51:18 +0200

    hw 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,15 @@
-
-// export function fetchCountries(countryName){
-//     fetch(`https://restcountries.com/v3.1/name/Ukraine`).then(response =>
-// { return response.json()
-// }).then(country => {
-//     console.log((country));
-//     const markup = template(country)
-//     console.log(markup);
-//     countriesList.innerHTML = markup
-// }).catch(error => {
-//     console.log(error);
-// })
-// }
+ export default function fetchCountries(countryName){
+     return fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(response =>
+     {
+         if (response.ok) {
+         
+             return response.json()
+             
+     }
+         else {
+            throw new Error("no country with such name exists")
+         }
+})
+}
  
-//  fetchCountries()
- 
+  
--- src/index.js
+++ src/index.js
@@ -2,24 +2,54 @@ import './css/styles.css';
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix'
 import template from './template.hbs'
-// import fetchCountries from './fetchCountries'
+import fetchCountries from './fetchCountries'
+import template2 from './template2.hbs'
+ 
 
 const countriesList = document.querySelector('.country-list')
+const input = document.querySelector('#search-box')
+const countryDetails = document.querySelector('.country-info')
 const DEBOUNCE_DELAY = 300;
-console.log(countriesList);
+input.addEventListener('input', debounce(onInputType, DEBOUNCE_DELAY))
 
 
- function fetchCountries(countryName){
-    fetch(`https://restcountries.com/v3.1/name/Ukraine`).then(response =>
-{ return response.json()
-})
+function onInputType(event){
+    let countryName = event.target.value
+    let trimCountry = countryName.trim()
+ 
+    if (countryName === "") {
+        countriesList.innerHTML = ""
+        countryDetails.innerHTML=""
+    }
+ 
+    else {
+        fetchCountries(trimCountry).then(renderCountriesList).catch(error => {
+    Notiflix.Notify.failure("Oops, there is no country with that name");  
+    })
+              
+  }
+    
 }
+
  
-fetchCountries().then(renderMarkup).catch(error => {
-    console.log(error);
-})
- 
-function renderMarkup(country) {
+function renderCountriesList(country) {
+    console.log(country)
+    if (country.length > 10) {
+        ;
+         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+    }
+    if (country.length >=2 && country.length <=10) {
         const markup = template(country)
-     countriesList.innerHTML = markup
-}
+        countriesList.innerHTML = markup
+        countryDetails.innerHTML=""
+   
+    }
+    if (country.length === 1) {
+        countriesList.innerHTML = ""
+        const detailsMarkup = template2(country)
+        countryDetails.innerHTML = detailsMarkup
+    }
+
+    }
+ 
+ 
